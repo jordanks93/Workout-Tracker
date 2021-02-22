cc0bb26d6a4032c3732cdb3ff97409fafe731eb2
Jordanks93 <69221166+jordanks93@users.noreply.github.com>
2021-02-22 12:56:51 -0500

    updated server file with route connections

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
+// Dependencies
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+// Port
 const PORT = process.env.PORT || 3000;
 
-//const User = require("./Model.js");
-
+// Setup Express server
 const app = express();
 
+// Enable morgan log
 app.use(logger("dev"));
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/", { useNewUrlParser: true });
+// Mongo DB connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+});
+
+// Link api and html routes
+require("./routes/html_routes.js")(app);
+require("./routes/api_routes.js")(app);
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+  console.log(`App running on (http://localhost:${PORT})`);
 });
